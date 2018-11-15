cc0c67302877185cb8ed0c398395337f143a620a
Andrew Park <13parksangjoon@gmail.com>
2018-11-14 16:28:21 -0800

    initial commit

---
+++ app/models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+    local: {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        firstName: {
+            type: String,
+            default: ''
+        },
+        lastName: {
+            type: String,
+            default: ''
+        }
+    }
+});
+
+UserSchema.methods.serialize = function() {
+    return {
+        username: this.username || '',
+        firstName: this.firstName || '',
+        lastName: this.lastName || ''
+    };
+};
+
+// generating a hash
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = { User };
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://localhost/symphony-forum-app' 
+
+};
